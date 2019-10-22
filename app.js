063a0e28155cb060aad55362ce71aed1c5c9816a
Patriciavaldebenito <patricia.valdebenito@usach.cl>
2019-10-22 00:46:21 -0300

    Se crea configuracion para comandos ingresados en consola, esto en ruta ./config/yargs con casos : crear , listar, y actualizar

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-const argv = require('yargs').argv;
-console.log(argv);
+//const argv = require('yargs').argv;
+
+const argv = require('./config/yargs').argv;
+//console.log(argv);
+
+let comando = argv._[0];
+
+
+switch (comando) {
+
+    case 'crear':
+        console.log('crear por hacer');
+        break;
+
+    case 'listar':
+        console.log('Mostrar todas las tareas por hacer');
+        break;
+
+    case 'actualizar':
+        console.log('Actualizar una tarea por  hacer');
+        break;
+
+    default:
+        console.log(' El comando no es reconocido');
+}
---
+++ config/yargs.js
@@ -0,0 +1,31 @@
+// configurar los comandos 
+
+
+const argv = require('yargs')
+    .command('crear', 'crear un elemento por hacer', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Descripción de una tarea por hacer'
+        }
+    })
+
+    .command('actualizar', 'Actualizar el estado completado de una tarea ', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Descripción de una tarea por hacer'
+        }, 
+        completado: {
+            default: true,
+            alias: 'c',
+            desc: 'Marca como competado o pendiente la tarea'
+        }
+
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
